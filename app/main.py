2f0a7c169da6a28a12cf39f30a8f3528c099a8ae
BarToSick <nikitospetrenko51@gmail.com>
2022-08-06 15:00:23 +0300

    JS Python 1

---
+++ app/main.py
@@ -0,0 +1,56 @@
+from typing import Union
+import os
+
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static");
+
+
+class Order(BaseModel):
+    name: str
+    phone: str
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/ls")
+def read_item():
+    return os.listdir()
+
+
+@app.post("/server.php")
+async def orda(order:Order):
+    return {"name": order.name, "phone": order.phone}
+
+
+@app.post("/server.php")
+async def serv(name: str = Form(None), phone: str = Form(None)):
+    return {"name": name, "phone": phone}
+
+
+@app.post('/server.php')
+def some_post():
+    print(1)
+
+
+@app.get("/{file_path:path}")
+async def read_file(file_path: str):
+    return FileResponse('files/' + file_path)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5050)
